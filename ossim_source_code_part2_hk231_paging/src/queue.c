8ec5ce2a308fad87751c512cf44090785b54a4bf
justarandomnameduh <97799964+justarandomnameduh@users.noreply.github.com>
2023-04-25 13:37:46 +0700

    Write enqueue and dequeue
    Enqueue: add proc to tail
    Dequeue: swap pcb_t with lowest priority to tail, return tail and reduce size by 1

--- ossim_source_code_part2_hk231_paging/src/queue.c
+++ ossim_source_code_part2_hk231_paging/src/queue.c
@@ -9,12 +9,28 @@ int empty(struct queue_t * q) {
 
 void enqueue(struct queue_t * q, struct pcb_t * proc) {
         /* TODO: put a new process to queue [q] */
+        (q->proc)[q->size] = proc;
+        q->size++;
 }
 
 struct pcb_t * dequeue(struct queue_t * q) {
         /* TODO: return a pcb whose prioprity is the highest
          * in the queue [q] and remember to remove it from q
          * */
-	return NULL;
+        if(empty(q))
+                return NULL;
+        struct pcb_t* tmp = (q->proc)[0];
+        int swap_index = -1;
+        for(int i = 0; i < q->size; i++){
+                if(tmp->priority > (q->proc)[i]->priority){
+                        swap_index = i;
+                        tmp = (q->proc)[i];
+                }
+        }
+        if(swap_index != q->size - 1){
+                (q->proc)[swap_index] = (q->proc)[size-1];
+                (q->proc)[size-1] = tmp;
+        }
+        size--;
+        return (q->proc)[size];
 }
-
